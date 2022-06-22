f50bcb4ca20f43988b4c712596bfd1603949a816
iamdeveloper <iamdeveloper.me@gmail.com>
2022-06-22 18:18:47 +0530

    cicd test

--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics,viewsets
 from .models import Employee
-from .serializers import EmployeeSerializer
+from testapp.serializers import EmployeeSerializer
 
 
 # Create your views here.
